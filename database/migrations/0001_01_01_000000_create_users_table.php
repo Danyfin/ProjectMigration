66939b04b1ba0606c06b6161f16cccd8e9c2aeac
Danyfin <gdanil871@gmail.com>
2025-09-26 08:55:43 +0500

    create migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('middlename');
+            $table->string('lastname');
+            $table->string('login')->unique();
             $table->string('password');
+            $table->string('tel');
+            $table->string('role');
+            $table->string('email')->unique();
+
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
