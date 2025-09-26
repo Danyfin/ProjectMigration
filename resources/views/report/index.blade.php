d26b74cae41a693e4284101612d58f99a3a56476
Danyfin <gdanil871@gmail.com>
2025-09-26 09:29:09 +0500

    create html

---
+++ resources/views/report/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    
+    <title>Заявления</title>
+</head>
+<body>
+    <header>
+        <div class="header">
+            <h1 class="logo">НАРУШЕНИЙ<span>.НЕТ</span></h1>
+            <div class="account">
+                <p class="name"></p>
+                <button>выйти</button>
+            </div>
+        </div>
+    </header>
+    <main>
+        <button class="create-report">
+            Создать заявление
+        </button>
+        <div class="cards">
+            <div class="card">
+                <div class="date">
+
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+    
+</body>
+</html>
