f381d7ee628f89ac7d8b947ae1d3325e3a536c43
Danyfin <gdanil871@gmail.com>
2025-09-26 09:13:42 +0500

    create migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('login')->unique();
             $table->string('password');
             $table->string('tel');
-            $table->string('role');
+            $table->string('role')->nullable();
             $table->string('email')->unique();
 
             $table->timestamp('email_verified_at')->nullable();
